8dfd6b14aa13ba3299382134b54be4eca86ef5db
oguzkuseli <oguz.kuseli@hotmail.com>
2023-12-26 15:17:45 +0300

    Initial Commit

---
+++ Odev4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev4
+//
+//  Created by Oğuz Kuşeli on 26.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
